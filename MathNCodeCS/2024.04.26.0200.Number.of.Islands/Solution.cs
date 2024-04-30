0f466ac91cf8d8db6ecf009a42b5290c13a84a03
Edward Park <psh5566@gmail.com>
2024-04-30 14:03:08 -0400

    first dfs solution added

--- MathNCodeCS/2024.04.26.0200.Number.of.Islands/Solution.cs
+++ MathNCodeCS/2024.04.26.0200.Number.of.Islands/Solution.cs
@@ -7,6 +7,51 @@ public class Solution
 {
     public int NumIslands(char[][] grid)
     {
-        throw new NotImplementedException();
+        int rowSize = grid.Length;
+        int colSize = grid[0].Length;
+
+        var visited = new HashSet<(int, int)>();
+        int maxArea = 0;
+
+        for (int i = 0; i < rowSize; i++)
+        {
+            for (int j = 0; j < colSize; j++)
+            {
+                maxArea = Math.Max(maxArea, Dfs(i, j));
+            }
+        }
+
+        return maxArea;
+
+        int Dfs(int r, int c)
+        {
+            //out of scope
+            if (r < 0 || r >= rowSize || c < 0 || c >= colSize)
+            {
+                return 0;
+            }
+
+            //visited
+            if (visited.Contains((r, c)))
+            {
+                return 0;
+            }
+
+            //not land
+            if (grid[r][c] == 0)
+            {
+                return 0;
+            }
+
+            visited.Add((r, c));
+
+            int currArea = 1;
+            currArea += Dfs(r + 1, c);
+            currArea += Dfs(r - 1, c);
+            currArea += Dfs(r, c + 1);
+            currArea += Dfs(r, c - 1);
+
+            return currArea;
+        }
     }
 }
