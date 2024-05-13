4047b4cf2ab43dd341117152218e30bde1044a29
Edward Park <psh5566@gmail.com>
2024-05-13 16:08:17 -0400

    add solution

--- MathNCodeCS/2024.05.09.1143.LongestCommonSubsequence/Solution.cs
+++ MathNCodeCS/2024.05.09.1143.LongestCommonSubsequence/Solution.cs
@@ -7,6 +7,23 @@ public class Solution
 {
     public int LongestCommonSubsequence(string text1, string text2)
     {
-        throw new NotImplementedException();
+        int l1 = text1.Length;
+        int l2 = text2.Length;
+
+        var memo = new Dictionary<(int, int), int>();
+
+        return Solve(0, 0);
+
+        int Solve(int i, int j)
+        {
+            if (i == l1 || j == l2)
+                return 0;
+            if (memo.ContainsKey((i, j)))
+                return memo[(i, j)];
+
+            int ret = text1[i] == text2[j] ? 1 + Solve(i + 1, j + 1) : Math.Max(Solve(i, j + 1), Solve(i + 1, j));
+            memo[(i, j)] = ret;
+            return ret;
+        }
     }
 }
