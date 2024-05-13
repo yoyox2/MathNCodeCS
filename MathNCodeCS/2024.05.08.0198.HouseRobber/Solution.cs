e7885265c31956749d9cd8b233d48284d1a445cd
Edward Park <psh5566@gmail.com>
2024-05-13 16:04:08 -0400

    add house robber solution

--- MathNCodeCS/2024.05.08.0198.HouseRobber/Solution.cs
+++ MathNCodeCS/2024.05.08.0198.HouseRobber/Solution.cs
@@ -7,6 +7,29 @@ public class Solution
 {
     public int Rob(int[] nums)
     {
-        throw new NotImplementedException();
+        int size = nums.Length;
+        if (size <= 0)
+            return 0;
+
+        var memo =
+            new Dictionary<int, int>
+            {
+                { size - 1, nums[^1] },
+                { size, 0 }
+            };
+
+        return Solve(0);
+
+        int Solve(int start)
+        {
+            if (memo.TryGetValue(start, out int value))
+                return value;
+
+            int rob = nums[start] + Solve(start + 2);
+            int skip = Solve(start + 1);
+            int max = Math.Max(rob, skip);
+            memo[start] = max;
+            return max;
+        }
     }
 }
